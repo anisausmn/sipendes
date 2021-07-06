c15c54c4f35496723a78742960c60c83f5d32d97
sulthonuladib <sulthonuladib@github.com>
2021-07-07 06:23:35 +0700

    Update: Admin section done

--- src/sipendes/SiPenDes.java
+++ src/sipendes/SiPenDes.java
@@ -21,8 +21,21 @@ public class SiPenDes {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        AdminControllerImplementation tester = new AdminControllerImplementation();
+        
+//        Admin adib = tester.getAllAdmin();
+        
+        List<Admin> testGetAllAdmin = tester.getAllAdmin();
+        for (Admin testAdmin : testGetAllAdmin) {
+            System.out.println( testAdmin.adminName);
+            System.out.println( testAdmin.adminAddress);
+            System.out.println( testAdmin.adminContact);
+            System.out.println( testAdmin.adminUsername);
+            System.out.println( testAdmin.adminPassword);
+            System.out.println( testAdmin.adminEmail);
+        }
         
     }
     
